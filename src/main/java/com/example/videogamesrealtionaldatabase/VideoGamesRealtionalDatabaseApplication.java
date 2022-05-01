77ae2475f46c223a805cbedba5281d2692844ead
Juan Sebastian <jsfc2199@gmail.com>
2022-05-01 14:13:58 -0500

    Start of the spring boot games api

---
+++ src/main/java/com/example/videogamesrealtionaldatabase/VideoGamesRealtionalDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.videogamesrealtionaldatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGamesRealtionalDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGamesRealtionalDatabaseApplication.class, args);
+	}
+
+}
